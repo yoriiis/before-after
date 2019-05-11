fbb9427468559e4b5c100d40723093d35245a0d4
Yoriiis <joris.daniel@gmail.com>
2019-05-11 02:10:32 +0200

    Update to 2.0.0

--- readme.md
+++ readme.md
@@ -1,86 +1,123 @@
-# jQuery beforeAfter
+![BeforeAfter](https://img.shields.io/badge/BeforeAfter-v2.0.0-b8e986.svg?style=flat-square)
 
-New version of beforeAfter plugin, more options, responsive to resize and new method to add preview of animation.<br />
+# BeforeAfter
 
-### Structure of HTML
+BeforeAfter is a lightweight Javascript library to compare images in before/after view. The new version of the library does not use any dependencies.
 
-Images must be superposed, and CSS class `active` determine the upper image.
-```html
-<ul class="beforeafter">
-    <li class="active"><img src="img/1.jpg" /></li>
-    <li><img src="img/2.jpg" /></li>
-</ul>
+## Installation
+
+BeforeAfter is available as the `before-after` package on <a href="https://www.npmjs.com/package/before-after" title="npm before-after">npm</a>.
+
+```
+npm install before-after --save
 ```
 
-### Librairies compatibilities
+## How it works
 
-BeforeAfter plugin is compatible with <a href="https://github.com/Modernizr/Modernizr" title="Modernizr" target="_blank">Modernizr</a> and <a href="https://github.com/desandro/imagesloaded" title="imagesLoaded" target="_blank">imagesLoaded</a>.<br />If one or the other plugins is unavailable, options are disable by default, and the plugin is fully functional.<br />For information, jQuery 1.7+ is require to use this plugin.
+### HTML structure
 
+Images must be superposed, and CSS class `beforeafter-itemActive` determine the upper image.
+
+```html
+<div class="beforeafter">
+    <div class="beforeafter-item beforeafter-itemActive">
+        <img src="img/1.jpg" />
+    </div>
+    <div class="beforeafter-item">
+        <img src="img/2.jpg" />
+    </div>
+</div>
+```
+
+### Basic usage
+
+Every page that contains BeforeAfter, has to instanciates them. The following example create one item.
+
+```javascript
+import BeforeAfter from 'before-after';
+const beforeAfterItem = new BeforeAfter({
+    element: document.querySelector('.beforeafter')
+});
+beforeAfterItem.create();
+```
 
 ### Options
 
-* `cursor` <span> tag following mouse or finger
-* `direction` 4 directions available : ltr | rtl | ttb | btt
-* `classNameCursor` class name for the tag cursor
-* `checkImagesLoaded` use jQuery imagesLoaded to detect all images loaded
-* `useCSSTransform` use Modernizr.testProp() to test compatibility with CSS transform
-* `debug` enable log in plugin
-* `callback` function called when the plugin is instanciated
-
-(ltr : left to right | rtl : right to left | ttb : top to bottom | btt : bottom to top)
-
-
-### Instanciate the plugin
+You can pass configuration options to BeforeAfter constructor. Example below show all default values. Allowed values are as follows:
 
 ```javascript
-$('.beforeafter').beforeafter({
+{
+    element: null,
     cursor: true,
     direction: 'ltr',
-    classNameCursor: 'cursor',
-    checkImagesLoaded: false,
-    useCSSTransform: true,
-    debug: false,
-    callback: function(){}
-});
-```
-
-### Control the preview
-
-You can start a preview of animation on callback function or when you want, with goTo method, follow example above and below.<br /><br />Use this parameters to control animation :
-
-* **percentage** : percentage of shifting (no unity)
-* **duration** : duration of animation (in ms)
-* **animation** : animate or not
-* **easing** : type of easing (available in your jQuery library)
-
-Example on click event
-```javascript
-$('.button').on('click', function(e){
-    e.preventDefault();
-    $('.beforeafter').data('beforeafter').goTo(50, 1000, true, 'swing');
-})
-```
-
-Example on init callback, `this` access to plugin instance
-```javascript
-$('.beforeafter').beforeafter({
-    callback: function(){
-        this.goTo(50, 1000, true, 'swing');
+    selectors: {
+        item: '.beforeafter-item',
+        itemActive: '.beforeafter-itemActive',
+        cursor: '.beforeafter-cursor',
+        imageWrapper: '.beforeafter-wrapperImage',
     }
+}
+```
+
+* `element` - {Object} - DOM element reference
+* `cursor` - {Boolean} - DIV tag following mouse or finger
+* `direction` - {String} - 4 directions available: `ltr`, `rtl`, `ttb`, `btt`
+* `selectors` - {Object} - Configuration of selectors used by BeforeAfter
+    * `item` - {String} - Selector of each items
+    * `itemActive` - {String} - Selector of the active item
+    * `cursor` - {String} - Selector of the cursor
+    * `imageWrapper` - {String} - Selector of the picture wrapper (_automatically generated_)
+
+_Note: direction parameter uses naming shortcuts: `left to right`, `right to left`, `top to bottom`,  `bottom to top`_
+
+## Callback functions
+
+There is one callback function available with BeforeAfter.
+
+### After the instanciation
+
+The `create` method is a function which accepts a callback function in parameter.
+
+```javascript
+import BeforeAfter from 'before-after';
+const beforeAfterItem = new BeforeAfter({
+    element: document.querySelector('.beforeafter')
+});
+beforeAfterItem.create(() => {
+    // BeforeAfter element is created and ready
 });
 ```
 
-### Methods
+## Available methods
 
-* `reset` reset CSS property
-* `destroy` reset and destroy plugin instance
+Each BeforeAfter instanciations returns the instance of the class with somes available methods to easily manipulate the element.
 
-Access to method with instance
+### Go to a position
+
+The `goTo()` function allows to move the animation to a specific position. The function accepts one parameter `percentage` in percentage only (without unity).
+
+The function can be used in the callback of the `create()` function, to start the animation on a specific position.
+
 ```javascript
-$('.beforeafter').data('beforeafter').reset();
-$('.beforeafter').data('beforeafter').destroy();
+beforeAfterItem.goTo(50);
 ```
 
-### Browser support
+### Reset
 
-IE8+, FF, Chrome, Opera, Safari, iOS, Android, Window 8
+The `reset()` function allows to reset the animation and the cursor.
+
+```javascript
+beforeAfterItem.reset();
+```
+
+### Destroy
+
+The `destroy()` function allows to destroy all references of BeforeAfter and keep the original HTML structure.
+
+```javascript
+beforeAfterItem.destroy();
+```
+
+### Browsers support
+
+BeforeAfter is compatible with all no-touch and touch devices such as Android or iOS. The library uses CSS transform property for better animations, all last browsers are compatible. More information in <a href="https://caniuse.com/#search=transform" title="Can I Use">Can I Use</a>
---
+++ src/before-after.js
@@ -0,0 +1,460 @@
+/**
+ * @license MIT
+ * @name BeforeAfter
+ * @version 2.0.0
+ * @author: Yoriiis aka Joris DANIEL <joris.daniel@gmail.com>
+ * @description: BeforeAfter is a lightweight Javascript library to compare images in before/after view without any dependencies
+ * {@link https://github.com/yoriiis/before-after.js}
+ * @copyright 2019 Joris DANIEL
+ **/
+
+'use strict';
+
+class BeforeAfter {
+    /**
+     * @param {options}
+     */
+    constructor(options) {
+        const userOptions = options || {};
+        const defaultOptions = {
+            element: null,
+            cursor: true,
+            direction: 'ltr',
+            selectors: {
+                item: '.beforeafter-item',
+                itemActive: '.beforeafter-itemActive',
+                cursor: '.beforeafter-cursor',
+                imageWrapper: '.beforeafter-wrapperImage',
+            }
+        }
+
+        // Merge default options with user options
+        this.options = Object.assign(defaultOptions, userOptions);
+
+        // Detect touch devices
+        this.hasTouch = 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+        this.hasMSPointer = window.navigator.msPointerEnabled;
+
+        if (this.options.element === null) {
+            console.warn('BeforeAfter::Option element is missing');
+        }
+
+        this.itemActive = this.options.element.querySelector(this.options.selectors.itemActive);
+        this.orientation = '';
+        this.attrToAnimate = '';
+        this.widthHeight = '';
+        this.heightElement = null;
+        this.widthElement = null;
+        this.timerGoTo = null;
+        this.timerGoToAfter = null;
+        this.timerCleaned = false;
+        this.cursor = null;
+    }
+
+    /**
+     * Create the BeforeAfter item
+     * @param {Function} callback Callback function to excecute when BeforeAfter item is created
+     */
+    create(callback) {
+        // Check if item active exist
+        if (this.itemActive === null) {
+            console.warn('BeforeAfter::Item active is missing');
+        } else {
+
+            this.detectOrientation();
+            this.itemActive.style.zIndex = 1;
+
+            // Build the cursor if option is enabled
+            if (this.options.cursor) {
+                this.updateCursor();
+            }
+
+            // Check images loaded before wrap it
+            this.checkImagesLoaded(() => {
+                this.heightElement = parseInt(this.options.element.offsetHeight);
+                this.widthElement = parseInt(this.options.element.offsetWidth);
+                this.addImageWrapper();
+            });
+
+            this.addEvents();
+
+            // Excecute the callback function if it is available
+            if (typeof callback === 'function') {
+                callback();
+            }
+
+        }
+    }
+
+    /**
+     * Detect the orientation and somes attributes from the direction
+     */
+    detectOrientation() {
+
+        //Detect orientation and attribute to animate
+        if (this.options.direction === 'ltr' || this.options.direction === 'rtl') {
+            this.orientation = 'horizontal';
+            this.widthHeight = 'width';
+            this.attrToAnimate = this.options.direction === 'ltr' ? 'left' : 'right';
+        } else if (this.options.direction === 'ttb' || this.options.direction === 'btt') {
+            this.orientation = 'vertical';
+            this.attrToAnimate = this.options.direction === 'ttb' ? 'top' : 'bottom';
+            this.widthHeight = 'height';
+        }
+
+    }
+
+    /**
+     * Build the cursor item if it is not exist
+     */
+    buildCursor() {
+        const htmlCursor = `<div class='${this.options.selectors.cursor.substr(1)}'></div>`;
+        this.options.element.insertAdjacentHTML('beforeend', htmlCursor);
+    }
+
+    /**
+     * Update the cursor position on move events
+     */
+    updateCursor() {
+
+        // Build the cursor if it is not exist
+        if (this.cursor === null) {
+            this.buildCursor();
+        }
+
+        this.cursor = this.options.element.parentNode.querySelector(this.options.selectors.cursor);
+
+        this.cursor.style.position = 'absolute';
+        this.cursor.style.zIndex = 10;
+        this.cursor.style.backgroundColor = '#000';
+        this.cursor.style.overflow = 'hidden';
+        this.cursor.style.display = 'block';
+
+        if (this.orientation === 'horizontal') {
+            this.cursor.style.width = '2px';
+            this.cursor.style.height = '100%';
+            this.cursor.style.top = '0px';
+        } else if (this.orientation === 'vertical') {
+            this.cursor.style.width = '100%';
+            this.cursor.style.height = '2px';
+            this.cursor.style.left = '0px';
+        }
+
+        if (this.options.direction === 'ltr') {
+            this.cursor.style.left = '0px';
+        }
+        if (this.options.direction === 'rtl') {
+            this.cursor.style.left = 'auto';
+            this.cursor.style.right = '0px';
+        }
+        if (this.options.direction === 'ttb') {
+            this.cursor.style.top = '0px';
+        }
+        if (this.options.direction === 'btt') {
+            this.cursor.style.top = 'auto';
+            this.cursor.style.bottom = '0px';
+        }
+
+    }
+
+    /**
+     * Check images loadeds before excecute callback function
+     * @param {Function} callback Callback function to excecute on all images loaded
+     */
+    checkImagesLoaded(callback) {
+
+        const pictures = [...this.options.element.querySelectorAll('img')];
+        let counterLoaded = 0;
+
+        pictures.forEach(picture => {
+            let pictureTest = new Image();
+            pictureTest.src = picture.getAttribute('src');
+            pictureTest.onload = () => {
+                counterLoaded++;
+                if (counterLoaded === pictures.length - 1) {
+                    callback();
+                }
+            };
+        });
+
+    }
+
+    /**
+     * Wrap all images with a wrapper
+     */
+    addImageWrapper() {
+        const pictures = [...this.options.element.querySelectorAll('img')];
+        pictures.forEach(picture => {
+            const wrapper = document.createElement('div');
+            wrapper.classList.add(this.options.selectors.imageWrapper.substr(1));
+            picture.parentNode.insertBefore(wrapper, picture);
+            wrapper.appendChild(picture);
+
+            if (this.orientation === 'horizontal') {
+                wrapper.style.position = 'absolute';
+                wrapper.style.top = '0px';
+                wrapper.style.width = `${this.widthElement}px`;
+                this.itemActive.style.top = '0px';
+
+                if (this.options.direction === 'ltr') {
+                    this.itemActive.style.left = 'auto';
+                    this.itemActive.style.right = '0';
+                    wrapper.style.right = '0px';
+                }
+
+                if (this.options.direction === 'rtl') {
+                    this.itemActive.style.left = '0';
+                    this.itemActive.style.right = 'auto';
+                    wrapper.style.left = '0px';
+                }
+            } else if (this.orientation === 'vertical') {
+                wrapper.style.position = 'absolute';
+                wrapper.style.left = '0px';
+                wrapper.style.width = '100%';
+
+                if (this.options.direction === 'ttb') {
+                    this.itemActive.style.bottom = '0';
+                    this.itemActive.style.top = 'auto';
+                    wrapper.style.bottom = '0px';
+                }
+
+                if (this.options.direction === 'btt') {
+                    this.itemActive.style.bottom = 'auto';
+                    this.itemActive.style.top = '0';
+                    wrapper.style.top = '0px';
+                }
+            }
+        });
+    }
+
+    /**
+     * Remove wrapper on each images
+     */
+    removeImageWrappers() {
+
+        const items = [...this.options.element.querySelectorAll(this.options.selectors.item)];
+        items.forEach(item => {
+            item.appendChild(item.querySelector('img'));
+            item.querySelector(this.options.selectors.imageWrapper).remove();
+            item.removeAttribute('style');
+        });
+
+    }
+
+    /**
+     * Add event listeners on move and resize
+     */
+    addEvents() {
+        this.onMove = e => {
+            this.move(e);
+        };
+        this.getUserEventsToTrack().forEach(event => {
+            this.options.element.addEventListener(event, this.onMove, false);
+        });
+
+        this.onResize = () => {
+            this.resize();
+        };
+        window.addEventListener('resize', this.onResize, false);
+    }
+
+    /**
+     * Remove event listeners on move and resize
+     */
+    removeEvents() {
+        this.getUserEventsToTrack().forEach(event => {
+            this.options.element.removeEventListener(event, this.onMove);
+        });
+
+        window.removeEventListener('resize', this.onResize);
+    }
+
+    /**
+     * Detect which events to track depending on the capabilities of the device
+     * @returns {Array} with the list of events
+     */
+    getUserEventsToTrack() {
+        return this.hasTouch ? (this.hasMSPointer ? ['pointerstart', 'MSPointerMove'] : ['touchstart', 'touchmove']) : ['mousemove'];
+    }
+
+    /**
+     * Update position on resize event
+     */
+    resize() {
+
+        let valueMoveCSS = 0;
+        let valueMoveTransform = null;
+
+        this.heightElement = parseInt(this.options.element.offsetHeight);
+        this.widthElement = parseInt(this.options.element.offsetWidth);
+
+        if (this.orientation === 'horizontal') {
+
+            const beforeAfterItems = [...this.options.element.querySelectorAll(this.options.selectors.itemActive)];
+            beforeAfterItems.forEach(item => {
+                item.style.position = 'absolute';
+                item.style.top = '0px';
+                item.style.width = this.widthElement;
+            });
+
+            if (this.options.direction === 'ltr') {
+                valueMoveCSS = this.itemActive.offsetLeft;
+                valueMoveTransform = `${valueMoveCSS}px, 0`;
+            } else if (this.options.direction === 'rtl') {
+                valueMoveCSS = (this.widthElement - this.itemActive.offsetWidth) * -1;
+                valueMoveTransform = `${valueMoveCSS}px, 0`;
+            }
+
+        } else if (this.orientation === 'vertical') {
+
+            if (this.options.direction === 'ttb') {
+                valueMoveCSS = this.itemActive.offsetTop;
+                valueMoveTransform = `0, ${valueMoveCSS}px`;
+            } else if (this.options.direction === 'btt') {
+                valueMoveCSS = (this.heightElement - this.itemActive.offsetHeight) * -1;
+                valueMoveTransform = `0 , ${valueMoveCSS}px`;
+            }
+
+        }
+
+        if (this.options.cursor) {
+            this.cursor.style.transform = `translate(${valueMoveTransform}) translateZ(0)`;
+        }
+
+    }
+
+    /**
+     * Move images and cursor on user events
+     * @param {Object} e Object from event listener
+     */
+    move(e) {
+        e.preventDefault();
+
+        //If user hover during animation, clear timer and stop animate
+        if (this.timerGoTo !== null && !this.timerCleaned) {
+            clearTimeout(this.timerGoTo);
+            clearTimeout(this.timerGoToAfter);
+            if (this.options.cursor) {
+                this.updateCursor();
+            }
+            this.timerCleaned = true;
+        }
+
+        let valueMoveTransform = 0;
+        let valueMoveCSS = 0;
+        let valueMovePicture = 0;
+        let pageX = 0;
+        let pageY = 0;
+
+        pageX = this.hasTouch ? e.touches[0].pageX : (this.hasMSPointer ? e.pageX : e.pageX);
+        pageY = this.hasTouch ? e.touches[0].pageY : (this.hasMSPointer ? e.pageY : e.pageY);
+        const elementBoundingClientRect = this.options.element.getBoundingClientRect();
+
+        if (this.options.direction === 'ltr' || this.options.direction === 'rtl') {
+            if (this.options.direction === 'ltr') {
+                valueMoveCSS = parseInt(pageX - elementBoundingClientRect.x);
+                valueMoveTransform = valueMoveCSS + 'px, 0px';
+                valueMovePicture = this.widthElement - valueMoveCSS;
+            } else {
+                valueMoveCSS = parseInt(this.widthElement - (pageX - elementBoundingClientRect.x));
+                valueMoveTransform = -valueMoveCSS + 'px, 0px';
+                valueMovePicture = this.widthElement - valueMoveCSS;
+            }
+        } else if (this.options.direction === 'ttb' || this.options.direction === 'btt') {
+            if (this.options.direction === 'ttb') {
+                valueMoveCSS = parseInt(pageY - elementBoundingClientRect.y);
+                valueMoveTransform = '0px, ' + valueMoveCSS + 'px';
+                valueMovePicture = this.heightElement - valueMoveCSS;
+            } else {
+                valueMoveCSS = parseInt(this.heightElement - (pageY - elementBoundingClientRect.y));
+                valueMoveTransform = '0px, ' + -valueMoveCSS + 'px';
+                valueMovePicture = this.heightElement - valueMoveCSS;
+            }
+        }
+
+        // If cursor enabled, apply new position
+        if (this.options.cursor) {
+            this.cursor.style.transform = `translate(${valueMoveTransform}) translateZ(0)`;
+        }
+
+        // Update new position on image
+        this.itemActive.style[this.widthHeight] = `${valueMovePicture}px`;
+
+        //Update current position available on instance
+        this.position = valueMoveCSS;
+    }
+
+    /**
+     * Move the animation of the image and cursor to a specific position
+     * @param {Interger|Float} percentage Percentage of offset
+     */
+    goTo(percentage) {
+        this.timerGoTo = setTimeout(() => {
+            let valueMoveDependOnElement = 0;
+            let valueMove = 0;
+            let valueCursorTransform = 0;
+
+            if (this.options.direction === 'ltr' || this.options.direction === 'rtl') {
+                valueMoveDependOnElement = this.widthElement - (this.widthElement * percentage) / 100;
+                valueMove = (this.widthElement * percentage) / 100;
+            } else if (this.options.direction === 'ttb' || this.options.direction === 'btt') {
+                valueMoveDependOnElement = this.heightElement - (this.heightElement * percentage) / 100;
+                valueMove = (this.heightElement * percentage) / 100;
+            }
+
+            this.itemActive.style[this.widthHeight] = `${valueMoveDependOnElement}px`;
+
+            if (this.options.cursor) {
+                this.cursor.style[this.attrToAnimate] = valueMove;
+
+                if (this.options.direction === 'ltr') {
+                    valueCursorTransform = valueMoveDependOnElement + 'px, 0';
+                    this.cursor.style[this.attrToAnimate] = 'auto';
+                } else if (this.options.direction === 'rtl') {
+                    valueCursorTransform = valueMoveDependOnElement + 'px, 0';
+                    this.cursor.style[this.attrToAnimate] = 'auto';
+                } else if (this.options.direction === 'ttb') {
+                    valueCursorTransform = '0, ' + valueMoveDependOnElement + 'px';
+                    this.cursor.style[this.attrToAnimate] = 0;
+                } else if (this.options.direction === 'btt') {
+                    this.cursor.style[this.attrToAnimate] = 'auto';
+                    valueCursorTransform = '0, ' + valueMoveDependOnElement + 'px';
+                }
+
+                this.cursor.style.transform = `translate(${valueCursorTransform}) translateZ(0)`;
+            }
+
+            //Update current position available on instance
+            this.position = valueMoveDependOnElement;
+        }, 100);
+    }
+
+    /**
+     * Reset the BeforeAfter item (image and cursor)
+     */
+    reset() {
+        this.cursor.style.transform = 'none';
+
+        if (this.orientation === 'horizontal') {
+            this.itemActive.style.width = '100%';
+            this.cursor.style.left = '0px';
+        } else {
+            this.itemActive.style.height = '100%';
+            this.cursor.style.top = '0px';
+        }
+    }
+
+    /**
+     * Remove the BeforeAfter item
+     */
+    destroy() {
+
+        this.reset();
+        this.cursor.remove();
+        this.removeEvents();
+        this.removeImageWrappers();
+
+    }
+}
+
+export default BeforeAfter;
